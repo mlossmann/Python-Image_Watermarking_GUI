f74d1320d653f3154fd40ab329fbc3b4c3029afe
mike <michaellossmann@gmail.com>
2023-01-27 11:25:56 -0500

    Added Application Data

---
+++ main.py
@@ -0,0 +1,11 @@
+from PIL import Image
+import tkinter as tk
+
+# tkinter config
+window = tk.Tk()
+window.title("Image Watermarking Application")
+header = tk.Label(text="Image Watermarking Application")
+header.pack()
+
+
+window.mainloop()
